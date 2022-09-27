6376b41c3ece796aefff33c98f8148a7a6656225
Josué Ayala <104921046+jayalaa@users.noreply.github.com>
2022-09-27 13:15:58 -0600

    ✅ Add new meeting routes

--- src/routes/meeting.route.js
+++ src/routes/meeting.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { storeMeeting } from '../controllers/meeting.controller';
+import { showMeeting, storeMeeting } from '../controllers/meeting.controller';
 import auth from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/', auth, storeMeeting);
+router.get('/:id', auth, showMeeting);
 
 export default router;
--- src/services/meeting.service.js
+++ src/services/meeting.service.js
@@ -1,3 +1,5 @@
+import httpStatus from 'http-status';
+import createError from '../utils/createError';
 import Meeting from '../models/meeting.model';
 
 export const createMeeting = async ({ title, user_id }) => {
@@ -16,6 +18,16 @@ export const createMeeting = async ({ title, user_id }) => {
     ...service.toJSON(),
     user,
   };
+};
+
+export const searchMeeting = async (key) => {
+  const meeting = await Meeting.findByPk(key);
+
+  if (!meeting) {
+    throw createError(httpStatus.NOT_FOUND, "Meeting key doesn't valid.");
+  }
+
+  return meeting;
 };
 
 export const getMeetings = async (user) =>
