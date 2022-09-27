95fc9c54144c5538d44caa431db6821e2088da60
Josué Ayala <ayalavalencia11@gmail.com>
2022-09-27 13:12:26 -0600

    💫 Update `.env` variables import

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
-import path from 'path';
-
-dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const {
   DATABASE,
@@ -12,8 +8,20 @@ const {
   DATABASE_PORT,
 } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE}`
+const sequelize = new Sequelize(DATABASE, DATABASE_USER, DATABASE_PASSWORD, {
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  dialect: 'postgres',
+  logging: false,
+});
+
+sequelize.sync().then(
+  () => {
+    console.log('DB connection sucessful.');
+  },
+  (err) => {
+    console.log(err);
+  }
 );
 
 export default sequelize;
