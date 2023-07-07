4f875993e150900c3f0a58f3953342de04e62fa1
Josué Ayala <ayalavalencia11@gmail.com>
2023-07-06 19:26:20 -0600

    Add `user/me` entry point & middleware configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express, { Router } from 'express';
 
 import authRoute from './auth.route';
 import authProviderRouter from './auth-provider.route';
+import userRouter from './user.route';
 
 const router: Router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/auth-providers', authProviderRouter);
+router.use('/user', userRouter);
 
 export default router;
