c1339e42d8ddc9ce08a5e56156fe880dba28d974
Josué Ayala <ayalavalencia11@gmail.com>
2023-07-16 19:26:45 -0600

    Add `/me` endpoint logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import db from '../config/database';
 
-export const getMe = (req: Request, res: Response, next: NextFunction) => {
+export const getMe = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    res.json(req.user);
+    const user = await db
+      .selectFrom('users')
+      .select(['id', 'full_name', 'avatar', 'email', 'bio'])
+      .where('id', '=', req.user?.user_id as any)
+      .execute();
+    res.json(user);
   } catch (error) {
     next(error);
   }
